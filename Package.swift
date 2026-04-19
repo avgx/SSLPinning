a147b835fdfb53196eb1643365e5cf8110f9a02e
avgx <agovorovsky@gmail.com>
2026-04-19 23:37:19 +0300

    add platforms

--- Package.swift
+++ Package.swift
@@ -6,8 +6,11 @@ import PackageDescription
 let package = Package(
     name: "SSLPinning",
     platforms: [
-        .macOS(.v13),
         .iOS(.v15),
+        .tvOS(.v15),
+        .macOS(.v13),
+        .watchOS(.v9),
+        .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
