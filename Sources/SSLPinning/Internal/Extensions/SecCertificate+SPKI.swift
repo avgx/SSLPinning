3d3f1b90316c1c1a38a183d97a41a0e4d44d61b7
avgx <agovorovsky@gmail.com>
2026-06-16 14:06:20 +0300

    simplify fix

--- Sources/SSLPinning/Internal/Extensions/SecCertificate+SPKI.swift
+++ Sources/SSLPinning/Internal/Extensions/SecCertificate+SPKI.swift
@@ -6,11 +6,7 @@ import X509
 
 extension SecCertificate {
     func spkiHash(from x509: X509.Certificate? = nil) throws -> Data {
-        let certificate = try x509 ?? x509Certificate()
-        var serializer = DER.Serializer()
-        try serializer.serialize(certificate.publicKey)
-        let spkiDER = serializer.serializedBytes
-        return Data(SHA256.hash(data: Data(spkiDER)))
+        Data(SHA256.hash(data: try spkiDER(from: x509)))
     }
 
     func spkiDER(from x509: X509.Certificate? = nil) throws -> Data {
